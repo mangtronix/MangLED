cf1418ccca9ef7139ea72d7e0611d384311fe448
Michael Ang <mangtronix@gmail.com>
2019-02-13 19:51:52 +0100

    Allow setting private config through external defines, build envs, etc

--- mled-node/src/mled-node/private_config.h
+++ mled-node/src/mled-node/private_config.h
@@ -1,11 +1,20 @@
 
 // Define your SSID and password here. Don't check in the file to source control.
 
+// $$$ TODO - store settings e.g. in SPIFFS where it could be edited through web interface
+
+#ifndef WIFI_SSID
 #define WIFI_SSID "mled-private"
+#endif
+
+#ifndef WIFI_PASSWORD
 #define WIFI_PASSWORD "lightitup"
+#endif
 
 // This sets the node number which corresponds to:
 // - host name, e.g. mled-1
 // - MIDI channel name, e.g. 1
+#ifndef NODE_NUMBER
 #define NODE_NUMBER 1
+#endif
 
