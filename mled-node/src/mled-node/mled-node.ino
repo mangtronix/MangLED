cea77be9c23dfe345aac453deaf8f61877c7926b
Michael Ang <mangtronix@gmail.com>
2019-02-13 02:06:03 +0100

    Receive connections via Apple MIDI and blink status LED as notes are received

--- mled-node/src/mled-node/mled-node.ino
+++ mled-node/src/mled-node/mled-node.ino
@@ -1,6 +1,16 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
 
+#include "AppleMidi.h"
+APPLEMIDI_CREATE_INSTANCE(WiFiUDP, AppleMIDI);
+bool gMidiIsConnected = false;
+
+// Forward declaration of our Apple MIDI event handlers
+void OnAppleMidiConnected(uint32_t ssrc, char* name);
+void OnAppleMidiDisconnected(uint32_t ssrc);
+void OnAppleMidiNoteOn(byte channel, byte note, byte velocity);
+void OnAppleMidiNoteOff(byte channel, byte note, byte velocity);
+void OnAppleMidiControlChange(byte channel, byte controller, byte value);
 
 // Define your wifi SSID / pass in the external header file
 #include "private_config.h"
@@ -9,11 +19,15 @@ const char* gSSID = WIFI_SSID;
 const char* gPassword = WIFI_PASSWORD;
 const int gNodeNumber = NODE_NUMBER;
 
+#define STATUS_LED LED_BUILTIN
+void statusLedOff();
+void statusLedOn();
+
 String gNodeName;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(STATUS_LED, OUTPUT);
+  statusLedOn();
 
   Serial.begin(115200);
   Serial.println();
@@ -46,16 +60,19 @@ void setup() {
   // Set up our mled-n.local hostname
   setupBonjour();
 
+  yield();
+
+  // Set up a receiving AppleMIDI session
+  setupAppleMidi();
+
 
   Serial.println("Finished setup, here we go!");
+  statusLedOff();
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(500);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(500);
-  Serial.print(".");
+  // Process Apple MIDI messages
+  AppleMIDI.run();
 
 }
 
@@ -76,4 +93,89 @@ void setupBonjour() {
     }
   }
   Serial.println("mDNS responder started");
+}
+
+void setupAppleMidi() {
+  // Create a receiving session - the remote master needs to connect to us
+
+  Serial.println("Starting AppleMIDI session");
+  AppleMIDI.begin(gNodeName.c_str());
+
+  // Connect event handlers
+  AppleMIDI.OnConnected(OnAppleMidiConnected);
+  AppleMIDI.OnDisconnected(OnAppleMidiDisconnected);
+
+  AppleMIDI.OnReceiveNoteOn(OnAppleMidiNoteOn);
+  AppleMIDI.OnReceiveNoteOff(OnAppleMidiNoteOff);
+
+  // AppleMIDI.OnReceiveControlChange(OnAppleMidiControlChange);
+
+  Serial.println("Open Audio Midi Setup on OSX, or rtpMIDI on Windows");
+  Serial.print("Add device named ");
+  Serial.print(gNodeName);
+  Serial.println(".local:5004 and press Connect");
+}
+
+
+// ====================================================================================
+// Event handlers for incoming MIDI messages
+// ====================================================================================
+
+// -----------------------------------------------------------------------------
+// rtpMIDI session. Device connected
+// -----------------------------------------------------------------------------
+void OnAppleMidiConnected(uint32_t ssrc, char* name) {
+  gMidiIsConnected  = true;
+  Serial.print(F("Connected to session "));
+  Serial.println(name);
+}
+
+// -----------------------------------------------------------------------------
+// rtpMIDI session. Device disconnected
+// -----------------------------------------------------------------------------
+void OnAppleMidiDisconnected(uint32_t ssrc) {
+  gMidiIsConnected  = false;
+  Serial.println(F("Disconnected"));
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void OnAppleMidiNoteOn(byte channel, byte note, byte velocity) {
+  statusLedOn();
+  
+  Serial.print(F("Incoming NoteOn from channel:"));
+  Serial.print(channel);
+  Serial.print(F(" note:"));
+  Serial.print(note);
+  Serial.print(F(" velocity:"));
+  Serial.print(velocity);
+  Serial.println();
+}
+
+// -----------------------------------------------------------------------------
+//
+// -----------------------------------------------------------------------------
+void OnAppleMidiNoteOff(byte channel, byte note, byte velocity) {
+  statusLedOff();
+
+  Serial.print(F("Incoming NoteOff from channel:"));
+  Serial.print(channel);
+  Serial.print(F(" note:"));
+  Serial.print(note);
+  Serial.print(F(" velocity:"));
+  Serial.print(velocity);
+  Serial.println();
+}
+
+
+// Utility functions
+void statusLedOn()
+{
+  digitalWrite(STATUS_LED, LOW);
+}
+
+void statusLedOff()
+{
+  digitalWrite(STATUS_LED, HIGH);
 }
